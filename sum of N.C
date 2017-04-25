40ee67b2a90f0498c581de137cca4c46828db3ed
shanmugapriya10797 <shanmugapriya10797@gmail.com>
2017-04-25 11:07:42 +0530

    Create sum of N.C

---
+++ sum of N.C
@@ -0,0 +1,13 @@
+# shanmugapriya
+#include<stdio.h>
+    int main()
+    {
+        int a,i,sum=0;
+        scanf("%d",&a);
+        for(i=0;i<=a;i++)
+        {
+            sum=sum+i;
+        }
+        printf("\n%d",sum);
+        return 0;
+    }
