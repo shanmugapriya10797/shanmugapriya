3199fc1e0399742a55a4d2d01df423831cba9e56
shanmugapriya10797 <shanmugapriya10797@gmail.com>
2017-04-25 11:57:23 +0530

    Create power.C

---
+++ power.C
@@ -0,0 +1,11 @@
+# shanmugapriya
+#include<stdio.h>
+#include<math.h>
+    int main()
+    {
+        int a,c,d;
+        scanf("%d",&a);
+        scanf("%d",&c);
+        d=pow(a,c);
+        printf("%d",d);
+    }
