9ec6fd888420483ae421be808b28b455f692434f
shanmugapriya10797 <shanmugapriya10797@gmail.com>
2017-04-25 10:21:19 +0530

    Create alphabet.C

---
+++ alphabet.C
@@ -0,0 +1,11 @@
+# shanmugapriya#include<stdio.h>
+    int main()
+{
+char a;
+scanf("%s",&a);
+if((a>='a')&&(a<='z')||(a>='A')&&(a<='Z'))
+    printf("Alphabet");
+        else
+            printf("Not an alphabet");
+        return 0;
+    }
