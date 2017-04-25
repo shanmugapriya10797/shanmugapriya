07400ac983baaae4031272093a589bde089204dd
shanmugapriya10797 <shanmugapriya10797@gmail.com>
2017-04-25 10:13:55 +0530

    Create vowel or cons.C

---
+++ vowel or cons.C
@@ -0,0 +1,11 @@
+# shanmugapriya#include<stdio.h>
+    int main()
+{
+char a;
+scanf("%s",&a);
+if((a=='a')||(a=='A')||(a=='e')||(a=='E')||(a=='i')||(a=='I')||(a=='o')||(a=='O')||(a=='u')||(a=='U'))
+printf("Vowel");
+else
+printf("consonent");
+return 0;
+}
