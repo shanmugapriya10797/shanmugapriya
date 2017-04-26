3a2fc900cfb8a13b873f1294ff35cef05fb2f00a
shanmugapriya10797 <shanmugapriya10797@gmail.com>
2017-04-26 14:51:49 +0530

    Create even bw intr.C

---
+++ even bw intr.C
@@ -0,0 +1,14 @@
+# shanmugapriya
+#include<stdio.h>
+    int main()
+    {
+        int a,b,i;
+        scanf("%d %d",&a,&b);
+     for(i=a;i<=b;i++)
+     {
+        if(i%2==0)
+        
+            printf("\n%d",i);
+        }
+        return 0;
+     }
