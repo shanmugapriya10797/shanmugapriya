9fdff10793b02f5129807878baa58263055f976e
shanmugapriya10797 <shanmugapriya10797@gmail.com>
2017-06-29 12:00:32 +0530

    Create swapodd&evenchar.C

---
+++ swapodd&evenchar.C
@@ -0,0 +1,18 @@
+# shanmugapriya
+#include <stdio.h> 
+
+#include <string.h> 
+int main()
+{ 
+    char str[20],tmp;
+    int i,j;
+    printf("\nEnter a string : "); 
+    scanf("%s",str);
+    for(i=0;i<strlen(str);i=i+2)
+    { tmp = str[i]; 
+     str[i] = str[i+1]; 
+     str[i+1] = tmp;
+    } 
+    printf("\nAfter Swap String : %s",str);
+   return 0;
+}
